ebbcd1dbc09f796b3fd885d863ea965095510c7c
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-01 06:16:21 +0000

    Solution for 2024 Day 1

--- 2024/src/main/java/info/jab/aoc/day1/Day1.java
+++ 2024/src/main/java/info/jab/aoc/day1/Day1.java
@@ -1,17 +1,86 @@
 package info.jab.aoc.day1;
 
 import info.jab.aoc.Day;
+import com.putoet.resources.ResourceLines;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day1 implements Day<Integer> {
 
     @Override
     public Integer getPart1Result(String fileName) {
-        throw new UnsupportedOperationException();
+        //Load file
+        var lines = ResourceLines.list(fileName);
+
+        //Load 2 lists
+        var list1 = new ArrayList<Integer>();
+        var list2 = new ArrayList<Integer>();
+        lines.stream()
+            .forEach(line -> {
+                var parts = line.split("   ");
+                list1.add(Integer.parseInt(parts[0]));
+                list2.add(Integer.parseInt(parts[1]));
+            });
+        //Sort
+        var list12 = list1.stream().sorted().toList();
+        var list22 = list2.stream().sorted().toList();
+
+        //Calculate distance
+        //Sum
+        AtomicInteger index = new AtomicInteger();
+        return list12.stream()
+            .mapToInt(element -> {
+                int param1 = element;
+                int param2 = list22.get(index.get());
+                int distance = param1 - param2;
+                index.addAndGet(+1);
+                return Math.abs(distance);
+            })
+            .sum();
+    }
+
+    public static int countOccurrences(int target, List<Integer> list) {
+        int count = 0;
+        for (int number : list) {
+            if (number == target) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
     public Integer getPart2Result(String fileName) {
-        throw new UnsupportedOperationException();
+        //Load file
+        var lines = ResourceLines.list(fileName);
+
+        //Load 2 lists
+        var list1 = new ArrayList<Integer>();
+        var list2 = new ArrayList<Integer>();
+        lines.stream()
+            .forEach(line -> {
+                var parts = line.split("   ");
+                list1.add(Integer.parseInt(parts[0]));
+                list2.add(Integer.parseInt(parts[1]));
+            });
+        //Sort
+        var list12 = list1.stream().sorted().toList();
+        var list22 = list2.stream().sorted().toList();
+
+        //Calculate ocurrences
+        //Sum
+        AtomicInteger index = new AtomicInteger();
+        return list12.stream()
+            .mapToInt(element -> {
+                int param1 = element;
+                //int param2 = list22.get(index.get());
+                int param2 = countOccurrences(param1, list22);
+                int distance = param1 * param2;
+                index.addAndGet(+1);
+                return distance;
+            })
+            .sum();
     }
 
 }
--- 2024/src/test/java/info/jab/aoc/day1/Day1Test.java
+++ 2024/src/test/java/info/jab/aoc/day1/Day1Test.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Disabled;
 
 class Day1Test {
 
-    @Disabled
     @Test
     void should_solve_day1_part1_with_sample() {
         //Given
@@ -18,7 +17,46 @@ class Day1Test {
         var result = day1.getPart1Result(fileName);
 
         //Then
-        then(result).isEqualTo(7);
+        then(result).isEqualTo(11);
+    }
+
+    @Test
+    void should_solve_day1_part1() {
+        //Given
+        String fileName = "/day1/day1-input.txt";
+
+        //When
+        Day1 day1 = new Day1();
+        var result = day1.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo(3574690);
+    }
+
+    @Test
+    void should_solve_day1_part2_with_sample() {
+        //Given
+        String fileName = "/day1/day1-input-sample.txt";
+
+        //When
+        Day1 day1 = new Day1();
+        var result = day1.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(31);
+    }
+
+    @Test
+    void should_solve_day1_part2() {
+        //Given
+        String fileName = "/day1/day1-input.txt";
+
+        //When
+        Day1 day1 = new Day1();
+        var result = day1.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(22565391);
     }
 
 }
