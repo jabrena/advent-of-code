e13db3af55e54832355157274b35bb1ad8ff3c37
jabrena <bren@juanantonio.info>
2024-12-03 17:47:13 +0000

    Adding template for AOC 2024 Day4

---
+++ 2024/src/main/java/info/jab/aoc/day4/Day4.java
@@ -0,0 +1,21 @@
+package info.jab.aoc.day4;
+
+import info.jab.aoc.Day;
+
+import com.putoet.resources.ResourceLines;
+
+/**
+ * https://adventofcode.com/2016/day/4
+ **/
+public class Day4 implements Day<Integer> {
+
+    @Override
+    public Integer getPart1Result(String fileName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Integer getPart2Result(String fileName) {
+        throw new UnsupportedOperationException();
+    }
+}
