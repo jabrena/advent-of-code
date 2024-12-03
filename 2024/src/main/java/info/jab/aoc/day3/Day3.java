f73e80017ca6748ce4c8105dfb407942d7861e9a
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-03 18:38:15 +0100

    Feature/2024day3 (#17)
    * Initial solution
    * Refactoring Day 3

--- 2024/src/main/java/info/jab/aoc/day3/Day3.java
+++ 2024/src/main/java/info/jab/aoc/day3/Day3.java
@@ -4,19 +4,64 @@ import info.jab.aoc.Day;
 
 import com.putoet.resources.ResourceLines;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * https://adventofcode.com/2024/day/3
  **/
-public class Day3
- implements Day<Integer> {
+public class Day3 implements Day<Integer> {
+
+    // Define the regex as a constant
+    private static final String REGEX = "mul\\((\\d+),(\\d+)\\)";
+    private static final String REGEX2 = "(mul\\((\\d+),(\\d+)\\)|don't\\(\\)|do\\(\\))";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+    private static final Pattern PATTERN2 = Pattern.compile(REGEX2);
 
     @Override
     public Integer getPart1Result(String fileName) {
-        throw new UnsupportedOperationException();
+        var input = ResourceLines.line(fileName);
+
+        Matcher matcher = PATTERN.matcher(input);
+
+        List<Integer> sum = new ArrayList<>();
+        while (matcher.find()) {
+            var param1 = Integer.parseInt(matcher.group(1));
+            var param2 = Integer.parseInt(matcher.group(2));
+            sum.add(param1 * param2);
+        }
+
+        return sum.stream().reduce(0, (a, b) -> a + b);
     }
 
     @Override
     public Integer getPart2Result(String fileName) {
-        throw new UnsupportedOperationException();
+        var input = ResourceLines.line(fileName);
+
+        Matcher matcher = PATTERN2.matcher(input);
+
+        boolean enabled = true;
+        List<Integer> sum = new ArrayList<>();
+        while (matcher.find()) {
+            String command = matcher.group(0);
+
+            if(command.equals("don't()")) {
+                enabled = false;
+            }
+            if(command.equals("do()")) {
+                enabled = true;
+            }
+
+            if (matcher.group(2) != null && matcher.group(3) != null) {
+                var param1 = Integer.parseInt(matcher.group(2));
+                var param2 = Integer.parseInt(matcher.group(3));
+                if(enabled) {
+                    sum.add(param1 * param2);
+                }
+            }
+        }
+        return sum.stream().reduce(0, (a, b) -> a + b);
     }
 }
--- 2024/src/test/java/info/jab/aoc/day3/Day3Test.java
+++ 2024/src/test/java/info/jab/aoc/day3/Day3Test.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import com.putoet.utils.Timer;
 
 class Day3Test {
 
@@ -18,7 +19,49 @@ class Day3Test {
         var result = day.getPart1Result(fileName);
 
         //Then
-        then(result).isEqualTo(2);
+        then(result).isEqualTo(161);
+    }
+
+    @Disabled
+    @Test
+    void should_solve_day3_part1() {
+        //Given
+        String fileName = "/day3/day3-input.txt";
+
+        //When
+        var day = new Day3();
+        var result = day.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo(157621318);
+    }
+
+    @Test
+    void should_solve_day3_part2_with_sample() {
+        //Given
+        String fileName = "/day3/day3-input-sample2.txt";
+
+        //When
+        var day = new Day3();
+        var result = day.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(48);
+    }
+
+    @Test
+    void should_solve_day3_part2() {
+        Timer.run(() -> {
+            //Given
+            String fileName = "/day3/day3-input.txt";
+
+            //When
+            var day = new Day3();
+            var result = day.getPart2Result(fileName);
+
+            //Then
+            then(result).isEqualTo(79845780);
+        });
     }
 
 }
