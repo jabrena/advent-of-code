24159848ca203a5e8795187587672459115a1303
jabrena <bren@juanantonio.info>
2024-12-08 18:07:56 +0000

    Uploading solutions for Day 8

--- 2024/src/main/java/info/jab/aoc/day8/AntennaMap.java
+++ 2024/src/main/java/info/jab/aoc/day8/AntennaMap.java
@@ -15,8 +15,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
