c193855f3ba77472bb53f170eca445024cbf67e6
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-14 18:55:12 +0000

    WIP

--- 2024/src/main/java/info/jab/aoc/day2/ReportAnalyzer2.java
+++ 2024/src/main/java/info/jab/aoc/day2/ReportAnalyzer2.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Gatherers;
 
+//WIP
 //https://docs.oracle.com/en/java/javase/22/core/stream-gatherers.html
 //Not working as expected
 public class ReportAnalyzer2 {
