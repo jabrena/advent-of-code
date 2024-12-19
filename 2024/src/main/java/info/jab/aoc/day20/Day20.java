84fcf0adabb35826111545f2f7891f5d6447a5e7
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-19 21:14:36 +0000

    Setup Day 20

---
+++ 2024/src/main/java/info/jab/aoc/day20/Day20.java
@@ -0,0 +1,21 @@
+package info.jab.aoc.day20;
+
+import info.jab.aoc.Day;
+
+import com.putoet.resources.ResourceLines;
+
+/**
+ * https://adventofcode.com/2024/day/20
+ **/
+public class Day20 implements Day<Integer> {
+
+    @Override
+    public Integer getPart1Result(String fileName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Integer getPart2Result(String fileName) {
+        throw new UnsupportedOperationException();
+    }
+}
