d0b4a2cdb29681d3415b9c0e488b8b12dd9734b2
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-29 20:34:15 +0100

    Adding new approaches

--- 2015/src/main/java/info/jab/aoc/day3/Day3.java
+++ 2015/src/main/java/info/jab/aoc/day3/Day3.java
@@ -2,12 +2,6 @@ package info.jab.aoc.day3;
 
 import info.jab.aoc.Day;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.putoet.grid.Point;
-import com.putoet.resources.ResourceLines;
-
 /**
  * https://adventofcode.com/2015/day/3
  */
@@ -15,62 +9,11 @@ public class Day3 implements Day<Integer> {
 
     @Override
     public Integer getPart1Result(String fileName) {
-        var line = ResourceLines.line(fileName);
-
-        Set<Point> visitedHouses = new HashSet<>();        
-        int x = 0, y = 0;
-        visitedHouses.add(new Point(x, y));
-        
-        for (char move : line.toCharArray()) {
-            switch (move) {
-                case '^' -> y++;
-                case 'v' -> y--;
-                case '>' -> x++;
-                case '<' -> x--;
-            }
-            visitedHouses.add(new Point(x, y));
-        }
-        
-        return visitedHouses.size();
+        return new Houses3().solvePartOne(fileName);
     }
 
     @Override
     public Integer getPart2Result(String fileName) {
-        var line = ResourceLines.line(fileName);
-        
-        Set<Point> visitedHouses = new HashSet<>();
-        // Posiciones para Santa y Robo-Santa
-        int santaX = 0, santaY = 0;
-        int roboX = 0, roboY = 0;
-        
-        // Ambos comienzan en la misma casa
-        visitedHouses.add(new Point(0, 0));
-        
-        // Procesar movimientos alternando entre Santa y Robo-Santa
-        for (int i = 0; i < line.length(); i++) {
-            char move = line.charAt(i);
-            // Determinar si es turno de Santa (índices pares) o Robo-Santa (índices impares)
-            if (i % 2 == 0) {
-                // Turno de Santa
-                switch (move) {
-                    case '^' -> santaY++;
-                    case 'v' -> santaY--;
-                    case '>' -> santaX++;
-                    case '<' -> santaX--;
-                }
-                visitedHouses.add(new Point(santaX, santaY));
-            } else {
-                // Turno de Robo-Santa
-                switch (move) {
-                    case '^' -> roboY++;
-                    case 'v' -> roboY--;
-                    case '>' -> roboX++;
-                    case '<' -> roboX--;
-                }
-                visitedHouses.add(new Point(roboX, roboY));
-            }
-        }
-        
-        return visitedHouses.size();
+        return new Houses3().solvePartTwo(fileName);
     }
 }
