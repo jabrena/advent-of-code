5befa5a8fa0f8b437a836e2b7fce95f7d0e97ace
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-29 19:50:24 +0100

    Initial setup for Day3

---
+++ 2015/src/main/java/info/jab/aoc/day3/Day3.java
@@ -0,0 +1,19 @@
+package info.jab.aoc.day3;
+
+import info.jab.aoc.Day;
+
+/**
+ * https://adventofcode.com/2015/day/3
+ */
+public class Day3 implements Day<Integer> {
+
+    @Override
+    public Integer getPart1Result(String fileName) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public Integer getPart2Result(String fileName) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
