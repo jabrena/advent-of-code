497355e3318d3d0636645324813376317b02e331
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-29 20:37:55 +0100

    Initial setup for Day3 (#65)
    * Initial setup for Day3
    * Initial solution
    * Adding new approaches
    * Updating table

--- 2015/README.md
+++ 2015/README.md
@@ -4,3 +4,4 @@
 |----|----------|-----------------------|-------------------------|------------------------------|
 | 1  | NA | [Lisp2](./src/main/java/info/jab/aoc/day1/Lisp2.java) | Enums | Data processing |
 | 2  | NA | [Present2](./src/main/java/info/jab/aoc/day2/Present2.java) |  | Data processing |
+| 3  | NA | [Houses3](./src/main/java/info/jab/aoc/day3/Houses3.java) |  | Direction |
---
+++ 2015/src/main/java/info/jab/aoc/day3/Houses2.java
@@ -0,0 +1,94 @@
+package info.jab.aoc.day3;
+
+import info.jab.aoc.Solver;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import com.putoet.grid.Point;
+import com.putoet.resources.ResourceLines;
+
+class Houses2 implements Solver<Integer> {
+    
+    enum Direction {
+        UP('^', 0, 1),
+        DOWN('v', 0, -1),
+        RIGHT('>', 1, 0),
+        LEFT('<', -1, 0);
+
+        private final char symbol;
+        private final int dx;
+        private final int dy;
+
+        Direction(char symbol, int dx, int dy) {
+            this.symbol = symbol;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        public static Direction fromSymbol(char symbol) {
+            return Stream.of(values())
+                    .filter(dir -> dir.symbol == symbol)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid direction symbol: " + symbol));
+        }
+
+        public int getDx() {
+            return dx;
+        }
+
+        public int getDy() {
+            return dy;
+        }
+    }
+
+    @Override
+    public Integer solvePartOne(String fileName) {
+        var line = ResourceLines.line(fileName);
+
+        Set<Point> visitedHouses = new HashSet<>();        
+        int x = 0, y = 0;
+        visitedHouses.add(new Point(x, y));
+        
+        for (char move : line.toCharArray()) {
+            Direction direction = Direction.fromSymbol(move);
+            x += direction.getDx();
+            y += direction.getDy();
+            visitedHouses.add(new Point(x, y));
+        }
+        
+        return visitedHouses.size();
+    }
+
+    @Override
+    public Integer solvePartTwo(String fileName) {
+        var line = ResourceLines.line(fileName);
+        
+        Set<Point> visitedHouses = new HashSet<>();
+        int santaX = 0, santaY = 0;
+        int roboX = 0, roboY = 0;
+        
+        visitedHouses.add(new Point(0, 0));
+        
+        for (int i = 0; i < line.length(); i++) {
+            char move = line.charAt(i);
+            // Determinar si es turno de Santa (índices pares) o Robo-Santa (índices impares)
+            if (i % 2 == 0) {
+                // Turno de Santa
+                Direction direction = Direction.fromSymbol(move);
+                santaX += direction.getDx();
+                santaY += direction.getDy();
+                visitedHouses.add(new Point(santaX, santaY));
+            } else {
+                // Turno de Robo-Santa
+                Direction direction = Direction.fromSymbol(move);
+                roboX += direction.getDx();
+                roboY += direction.getDy();
+                visitedHouses.add(new Point(roboX, roboY));
+            }
+        }
+        
+        return visitedHouses.size();
+    }
+}
