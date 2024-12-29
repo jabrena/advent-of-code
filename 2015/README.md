53a5124bb761a9194dd39d240f32f06de2279a87
Juan Antonio Breña Moral <bren@juanantonio.info>
2024-12-29 19:29:23 +0100

    Improving solution

--- 2015/README.md
+++ 2015/README.md
@@ -3,4 +3,4 @@
 | #  | Time     | Link                  | Techniques              | Category                     |
 |----|----------|-----------------------|-------------------------|------------------------------|
 | 1  | NA | [Lisp2](./src/main/java/info/jab/aoc/day1/Lisp2.java) | Enums | Data processing |
-| 2  | NA | [Day2](./src/main/java/info/jab/aoc/day2/Day2.java) |  | Data processing |
+| 2  | NA | [Present2](./src/main/java/info/jab/aoc/day2/Present2.java) |  | Data processing |
--- 2015/src/main/java/info/jab/aoc/day2/Day2.java
+++ 2015/src/main/java/info/jab/aoc/day2/Day2.java
@@ -1,9 +1,5 @@
 package info.jab.aoc.day2;
 
-import java.util.Arrays;
-
-import com.putoet.resources.ResourceLines;
-
 import info.jab.aoc.Day;
 
 /**
@@ -13,48 +9,11 @@ public class Day2 implements Day<Integer> {
 
     @Override
     public Integer getPart1Result(String fileName) {
-        var list = ResourceLines.list(fileName);
-        return list.stream()
-            .map(line -> {
-                String[] dimensions = line.split("x");
-                int l = Integer.parseInt(dimensions[0]);
-                int w = Integer.parseInt(dimensions[1]);
-                int h = Integer.parseInt(dimensions[2]);
-                
-                int side1 = l * w;
-                int side2 = w * h;
-                int side3 = h * l;
-                
-                int smallestSide = Math.min(Math.min(side1, side2), side3);
-                int surfaceArea = 2 * (side1 + side2 + side3);
-                
-                return surfaceArea + smallestSide;
-            })
-            .mapToInt(Integer::intValue)
-            .sum();
+        return new Present2().solvePartOne(fileName);
     }
 
     @Override
     public Integer getPart2Result(String fileName) {
-        var list = ResourceLines.list(fileName);
-        return list.stream()
-            .map(line -> {
-                String[] dimensions = line.split("x");
-                int l = Integer.parseInt(dimensions[0]);
-                int w = Integer.parseInt(dimensions[1]);
-                int h = Integer.parseInt(dimensions[2]);
-                
-                // Encontrar las dos dimensiones más pequeñas para el perímetro más corto
-                int[] sides = {l, w, h};
-                Arrays.sort(sides);
-                int smallestPerimeter = 2 * (sides[0] + sides[1]);
-                
-                // Calcular el volumen para el lazo
-                int volume = l * w * h;
-                
-                return smallestPerimeter + volume;
-            })
-            .mapToInt(Integer::intValue)
-            .sum();
+        return new Present2().solvePartTwo(fileName);
     }
 }
