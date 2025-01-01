b9ec1b7318490178afa3dc623cacd0b59e89db6c
Juan Antonio Breña Moral <bren@juanantonio.info>
2025-01-01 11:01:15 +0100

    Initial solution for Day7 (#73)
    * Initial solution for Day7
    * Initial refactoring
    * Initial second part
    * Minor change

---
+++ 2015/src/main/java/info/jab/aoc/day7/Day7.java
@@ -0,0 +1,16 @@
+package info.jab.aoc.day7;
+
+import info.jab.aoc.Day;
+
+public class Day7 implements Day<Integer> {
+
+    @Override
+    public Integer getPart1Result(String fileName) {
+        return new Circuit().solvePartOne(fileName);
+    }
+
+    @Override
+    public Integer getPart2Result(String fileName) {
+        return new Circuit().solvePartTwo(fileName);
+    }
+}
